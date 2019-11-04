eb11b1c991f38cab7ca2f12f35f18bf2688ee8fb
naari3 <naari.named@gmail.com>
2019-11-04 18:55:42 +0900

    make html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.ts",
+  output: {
+    path: __dirname + "/dist",
+    filename: "index_bundle.js"
+  },
   module: {
     rules: [
       {
@@ -11,5 +17,11 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts"]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "public/index.html"
+    })
+  ]
 };
